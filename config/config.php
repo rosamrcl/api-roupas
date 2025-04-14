f54cab0761c135bded62a01ba5f7e44b7a207e5f
Charles Acioli <85137610+CharlesAcioli@users.noreply.github.com>
2025-04-14 16:56:25 -0300

    a

--- config/config.php
+++ config/config.php
@@ -2,5 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'api-roupas');
+define('DB_CHARSET', 'utf8mb4');
 define('DB_USER', 'api-roupas');
 define('DB_PASS', 'L5SoRO6745Sa4ijO2IXEroLO8IHiyO');
