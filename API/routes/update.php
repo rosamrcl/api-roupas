8c386bcf6b5d3385c8781e8965c8b92a9c3a5a2d
Charles Acioli <85137610+CharlesAcioli@users.noreply.github.com>
2025-04-23 15:23:51 -0300

    a

--- API/routes/update.php
+++ API/routes/update.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Api Roupas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./resources/css/style.css">
+    <link rel="stylesheet" href="/APP/resources/css/style.css">
 </head>
 <body>
 <header class="header">    
