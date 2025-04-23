2180e9358737bc6c49ee2acf7e0e51aae2df29bb
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-23 14:45:54 -0300

    .

--- update.php
+++ update.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filhas de D.Helena</title>
+    <title>Api Roupas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./resources/css/style.css">
 </head>
@@ -34,32 +34,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </nav>
         <div id="menu-btn" class="fas fa-bars"></div>  
 </header>
-<section class="product">
-    <div class="box">
-    <h1 class="heading"> Produtos Cadastrados</h1>
-    <table cellpadding="8">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Quantidade</th>
-            <th>Descrição</th>                        <th>Preço</th>
-        </tr>
-        <?php foreach ($produtos as $produto): ?>
-            <tr>
-            <td><?= $produto['ID_produto'] ?></td>
-            <td><?= $produto['nome'] ?></td>
-            <td><?= $produto['descricao'] ?></td>
-            <td><?= $produto['quantidade'] ?></td>
-            <td><?= $produto['valor']?></td>            
-            <td>
-                <a class="btn" href="update.php?id=<?= $produto['nome'] ?>">Editar</a> 
-                <a class="delete-btn" href="delete.php?id=<?= $produto['nome'] ?>" onclick="return confirm('Deseja deletar?')">Deletar</a>
-            </td>
-            </tr>
-            <?php endforeach; ?>
-    </table>
-    </div>
-</section>
+    <section class="add-product">
+        <form method="post">
+            <h3>Detalhes do produto</h3>            
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" required maxlength="50" class="box" placeholder="Digite o nome do produto">
+            <label for="descricao">Descrição:</label>            
+            <input type="text" name="descricao" required maxlength="50" class="box" placeholder="Descreva o produto">              
+            <label for="quantidade">Quantidade:</label>
+            <input type="number" name="quantidade" required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite quantidade do produto">           
+            <label for="valor">Valor:</label>
+            <input type="number" name="valor" required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite o valor do produto">
+            <input type="submit" value="Adicionar livro" class="btn" name="add_product">
+        </form>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="./resources/js/index.js"></script>
     
 </body>
 </html>
