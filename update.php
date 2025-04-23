0d7bc82b905a271a0915f43569ad51482498ecff
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-23 14:44:52 -0300

    .

--- update.php
+++ update.php
@@ -16,11 +16,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<h2>Editar Usuário</h2>
-<form method="POST">
-    Nome: <input type="text" name="nome" value="<?= $produto['nome'] ?>" required><br><br>
-    Descrição: <input type="text" name="descricao" value="<?= $produto['descricao'] ?>" required><br><br>
-    Quantidade: <input type="number" name="quantidade" value="<?= $produto['quantidade'] ?>" required><br><br>
-    Valor: <input type="number" name="valor" value="<?= $produto['valor']?>" required><br><br>
-    <button type="submit">Atualizar</button>
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filhas de D.Helena</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./resources/css/style.css">
+</head>
+<body>
+<header class="header">    
+        <a href="#" class="logo">API Roupas</a>
+        <nav class="navbar">
+        <a href="./create.php">Adicionar produtos</a>
+        <a href="./index.php">Todos os produtos</a>          
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>  
+</header>
+<section class="product">
+    <div class="box">
+    <h1 class="heading"> Produtos Cadastrados</h1>
+    <table cellpadding="8">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Descrição</th>                        <th>Preço</th>
+        </tr>
+        <?php foreach ($produtos as $produto): ?>
+            <tr>
+            <td><?= $produto['ID_produto'] ?></td>
+            <td><?= $produto['nome'] ?></td>
+            <td><?= $produto['descricao'] ?></td>
+            <td><?= $produto['quantidade'] ?></td>
+            <td><?= $produto['valor']?></td>            
+            <td>
+                <a class="btn" href="update.php?id=<?= $produto['nome'] ?>">Editar</a> 
+                <a class="delete-btn" href="delete.php?id=<?= $produto['nome'] ?>" onclick="return confirm('Deseja deletar?')">Deletar</a>
+            </td>
+            </tr>
+            <?php endforeach; ?>
+    </table>
+    </div>
+</section>
+    
+</body>
+</html>
