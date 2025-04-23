0e420763e8fa4a6cfa5c36d6cc82e3a6d69332cc
Charles Acioli <85137610+CharlesAcioli@users.noreply.github.com>
2025-04-23 16:51:43 -0300

    ,.

--- API/functions.php
+++ API/functions.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 require_once 'db.php';
-=======
-require_once('./db.php');
->>>>>>> 0ef7e5957257d8d0da24a86c708ddc8fbe5b9f00
 
 function salvarEmJSON($dados) {
     file_put_contents('data.json', json_encode($dados, JSON_PRETTY_PRINT));
