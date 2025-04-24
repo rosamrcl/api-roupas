7587210efa11641385659052804a6cef28c2294d
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-24 16:40:22 -0300

    .

--- API/update.php
+++ API/update.php
@@ -11,7 +11,7 @@ if(!$produto){
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     atualizarProduto($id, $_POST['nome'], $_POST['descricao'], $_POST['quantidade'], $_POST['valor']);
-    header("Location: ../index.php");
+    header("Location: index.php");
     exit;
 }
 ?>
@@ -38,14 +38,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form method="post">
             <h3>Detalhes do produto</h3>            
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" required maxlength="50" class="box" placeholder="Digite o nome do produto">
+            <input type="text" name="nome" required maxlength="50" class="box" placeholder="Digite o nome do produto" value="<?= $produto['nome'] ?>"
             <label for="descricao">Descrição:</label>            
-            <input type="text" name="descricao" required maxlength="50" class="box" placeholder="Descreva o produto">              
+            <input type="text" name="descricao" required maxlength="50" class="box" placeholder="Descreva o produto" value="<?= $produto['descricao'] ?>">              
             <label for="quantidade">Quantidade:</label>
-            <input type="number" name="quantidade" required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite quantidade do produto">           
+            <input type="number" name="quantidade" required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite quantidade do produto" value="<?= $produto['quantidade'] ?>">           
             <label for="valor">Valor:</label>
-            <input type="number" name="valor" step="0.010"  required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite o valor do produto">            
-            <a class="btn" href="./index.php">Adicionar produto</a>
+            <input type="number" name="valor" step="0.010"  required maxlength="10" min="0" max="9999999999" class="box" placeholder="Digite o valor do produto" value="<?= $produto['valor'] ?>">            
+            <button class="btn" type="submit">Atualizar</button>
         </form>
     </section>
    
